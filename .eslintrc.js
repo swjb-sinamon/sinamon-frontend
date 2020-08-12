da8e406b0f5096371d6549dfd2c7c5a8df902ea8
SkyLightQP <combbm@naver.com>
2020-08-12 20:24:36 +0900

    chore: add eslint and prettier config files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['react-app', 'airbnb'],
+  rules: {
+    'arrow-body-style': 'off',
+    'jsx-quotes': 'off',
+    'comma-dangle': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-tag-spacing': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-alert': 'off',
+    'no-console': 'off',
+    'linebreak-style': 'off'
+  }
+};
