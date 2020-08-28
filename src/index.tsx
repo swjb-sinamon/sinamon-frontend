5d8421af93c4b1e7d301e0fb8cb746586bf9f707
SkyLightQP <combbm@naver.com>
2020-08-28 22:36:34 +0900

    feat: apply dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
 import * as serviceWorker from './serviceWorker';
 import Router from './router';
 
 import './styles/global.css';
+
+dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
