0dab35ba10676b64b67595c385f099396704314e
SkyLightQP <combbm@naver.com>
2020-08-28 22:21:40 +0900

    feat: create Router.tsx to routing pages
    - feat: create LoginPage.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Router from './router';
 
 import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
-
+    <Router/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LoginPage}/>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
