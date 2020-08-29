0e2a06d8654666bdc5086a34439125a91b96b6d8
SkyLightQP <combbm@naver.com>
2020-08-29 17:17:15 +0900

    feat: create Input and Label atomics

---
+++ src/atomics/Form/Input.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 300px;
+  height: 40px;
+  
+  background-color: white;
+  padding-left: 16px;
+  
+  border-radius: 3px;
+  border: 1px #788097 solid;
+`;
+
+export default Input;
---
+++ src/atomics/Form/Label.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: block;
+  font-size: 14px;
+  margin-bottom: 10px;
+  color: #788097;
+`;
+
+export default Label;
