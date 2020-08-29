e3a3715ab93a929fa82c17cf32e07c2c23d5fd93
SkyLightQP <combbm@naver.com>
2020-08-29 17:31:02 +0900

    feat: implement LoginPage page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Heading1 } from 'src/atomics/Typography/Heading';
+import { ReactComponent as Friends } from 'src/assets/friends.svg';
+import BlankLine from 'src/utils/BlankLine';
+import Label from 'src/atomics/Form/Label';
+import Input from 'src/atomics/Form/Input';
+import LoginButton from '../components/Login/LoginButton';
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+const StyledForm = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  grid-column: 2 / 3;
+`;
+
+const ButtonGroup = styled.div`
+  & button {
+    margin-right: 1rem;
+  }
+  
+  & button:last-child {
+    margin-right: 0;
+  }
+`;
+
+const StyledSVG = styled(Friends)`
+  margin-right: -10px;
+  grid-column: 1 / 2;
+`;
 
 const LoginPage: React.FC = () => {
   return (
-    <>
-    </>
+    <Container>
+      <GridContainer>
+        <StyledSVG width={600} height={600}/>
+        <StyledForm>
+          <div>
+            <Heading1>수정과 로그인</Heading1>
+
+            <BlankLine gap={30}/>
+
+            <Label>이메일</Label>
+            <Input placeholder="이메일" type="email"/>
+
+            <BlankLine gap={20}/>
+
+            <Label>비밀번호</Label>
+            <Input placeholder="비밀번호" type="password"/>
+
+            <BlankLine gap={30}/>
+            <LoginButton/>
+          </div>
+        </StyledForm>
+      </GridContainer>
+    </Container>
   );
 };
 
