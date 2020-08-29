7f2b9a2d093388f7a49930850efe2874e61d842c
SkyLightQP <combbm@naver.com>
2020-08-29 15:54:13 +0900

    feat: create Button and Heading atomics

---
+++ src/atomics/Button/index.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const SmallButton = styled.button`
+  width: 80px;
+  height: 22px;
+  background-color: #0057ff;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 12px;
+  
+  &:hover {
+    background-color: #004ede;
+  }
+`;
+
+export const MediumButton = styled.button`
+  width: 100px;
+  height: 30px;
+  background-color: #0057ff;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 14px;
+  
+  &:hover {
+    background-color: #004ede;
+  }
+`;
+
+export const HugeButton = styled.button`
+  width: 120px;
+  height: 40px;
+  background-color: #0057ff;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 16px;
+  
+  &:hover {
+    background-color: #004ede;
+  }
+`;
---
+++ src/atomics/Typography/Heading.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Heading1 = styled.h1`
+  font-size: 32px;
+  font-weight: 800;
+  font-family: 'NanumSquareRound', sans-serif;
+`;
+
+export const Heading2 = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+export const Heading3 = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+`;
