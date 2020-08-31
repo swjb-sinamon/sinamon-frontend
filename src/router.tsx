26a2dce5f8d91a1b42ddf0d8a35910532a1bb571
SkyLightQP <combbm@naver.com>
2020-08-31 16:52:21 +0900

    feat: add login and register router

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from 'src/pages/LoginPage';
+import RegisterPage from 'src/pages/RegisterPage';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
       </Switch>
     </BrowserRouter>
   );
