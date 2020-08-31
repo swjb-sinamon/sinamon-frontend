b031ec5d8e69443a248ab0ff9a6ce82e65782b7f
SkyLightQP <combbm@naver.com>
2020-08-31 16:26:14 +0900

    feat: create RegisterHeaderText and RegisterFooterText components

---
+++ src/components/Register/RegisterHeaderText.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Heading1, Heading3 } from 'src/atomics/Typography/Heading';
+
+const TitleContainer = styled.div`
+  text-align: center;
+  margin-top: 1rem;
+`;
+
+const StyledHeading3 = styled(Heading3)`
+  color: var(--color-button-hover);
+`;
+
+const RegisterHeaderText: React.FC = () => {
+  return (
+    <TitleContainer>
+      <Heading1>수정과 회원가입</Heading1>
+      <StyledHeading3>더 나은 학교 생활을 제공합니다</StyledHeading3>
+    </TitleContainer>
+  );
+};
+
+export default RegisterHeaderText;
