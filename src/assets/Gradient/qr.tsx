ffbcc7fa8472f939cea87a28b0e683cac579969a
SkyLightQP <combbm@naver.com>
2020-09-06 00:15:30 +0900

    feat: add svg asset about gradient

---
+++ src/assets/Gradient/qr.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GradientQR: React.FC = () => {
+  return (
+    <svg width="0" height="0">
+      <linearGradient id="Gradient__QRCode" gradientTransform="rotate(135)">
+        <stop stopColor="#43cbff" offset="10%" />
+        <stop stopColor="#9708cc" offset="100%" />
+      </linearGradient>
+    </svg>
+  );
+};
+
+export default GradientQR;
