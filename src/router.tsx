4f945cebacb3688cf1aea367e8d7cee239ff84b1
SkyLightQP <combbm@naver.com>
2020-09-06 00:17:25 +0900

    fix: add / router for main page

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from 'src/pages/LoginPage';
 import RegisterPage from 'src/pages/RegisterPage';
+import MainPage from 'src/pages/MainPage';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/" component={MainPage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/register" component={RegisterPage} />
       </Switch>
