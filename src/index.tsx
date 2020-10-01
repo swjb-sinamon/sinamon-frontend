32c5635000affc597f5bdfd5877da12732bcb7a0
combbm <combbm@naver.com>
2020-10-02 00:06:55 +0900

    feat: apply ReactNotification library

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import dotenv from 'dotenv';
+import ReactNotification from 'react-notifications-component';
 import * as serviceWorker from './serviceWorker';
 import Router from './router';
 
 import './styles/global.css';
+import 'react-notifications-component/dist/theme.css';
 
 dotenv.config();
 
 ReactDOM.render(
   <React.StrictMode>
+    <ReactNotification />
     <Router />
   </React.StrictMode>,
   document.getElementById('root')
