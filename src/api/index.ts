1364ca6bf9e7e6a980555827d7c324f355dc7a56
combbm <combbm@naver.com>
2020-10-02 00:07:30 +0900

    feat: implement api interceptors about error handling

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import showToast from '../utils/toast';
 
 const host = process.env.REACT_APP_API_HOST || 'http://localhost:8080';
 
@@ -6,5 +7,24 @@ const Api = axios.create({
   baseURL: host,
   withCredentials: true
 });
+
+Api.interceptors.response.use(
+  (res) => res,
+  (e) => {
+    if (!e.response.data) return Promise.reject(e);
+
+    const { success, error } = e.response.data;
+    const isValidatorError = e.response.status === 400;
+    const isErrorDataNullOrUndefined = !error;
+
+    if (success) return Promise.reject(e);
+    if (isErrorDataNullOrUndefined) return Promise.reject(e);
+    if (isValidatorError) return Promise.reject(e);
+
+    showToast(`🔥 ${error}`, 'danger');
+
+    return Promise.reject(e);
+  }
+);
 
 export default Api;
