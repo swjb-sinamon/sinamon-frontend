dea674bce5c7dfdde99deff9b1aeb8638035c6b3
combbm <combbm@naver.com>
2020-10-02 00:59:56 +0900

    feat: implement validator of register form

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import BlankLine from '../utils/BlankLine';
 import { HugeButton } from '../atomics/Button';
 import RegisterHeaderText from '../components/Register/RegisterHeaderText';
 import RegisterFooterText from '../components/Register/RegisterFooterText';
 import RegisterForm from '../components/Register/RegisterForm';
+import showToast from '../utils/toast';
 
 const Container = styled.div`
   display: flex;
@@ -32,7 +33,82 @@ const ButtonWrapper = styled.div`
   }
 `;
 
+interface RegisterState {
+  readonly email: string;
+  readonly password: string;
+  readonly passwordConfirm: string;
+  readonly fullName: string;
+  readonly grade: number;
+  readonly class: number;
+  readonly number: number;
+}
+
+interface CheckState {
+  readonly privacy: boolean;
+  readonly tos: boolean;
+}
+
 const RegisterPage: React.FC = () => {
+  const state = useState<RegisterState>({
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    fullName: '',
+    grade: 0,
+    class: 0,
+    number: 1
+  });
+
+  const check = useState<CheckState>({
+    privacy: false,
+    tos: false
+  });
+
+  const validator = (): boolean => {
+    const blankCount = Object.values(state[0]).filter(
+      (inputValue) =>
+        inputValue === 0 || (typeof inputValue === 'string' && inputValue.trim() === '')
+    );
+    if (blankCount.length > 0) {
+      showToast('❗ 빈칸이 있습니다.', 'danger');
+      return false;
+    }
+
+    const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (!emailRegex.test(state[0].email)) {
+      showToast('❗ 올바른 이메일이 아닙니다.', 'danger');
+      return false;
+    }
+
+    if (state[0].password.length < 6) {
+      showToast('❗ 비밀번호는 최소 6자리이어야 합니다.', 'danger');
+      return false;
+    }
+
+    if (state[0].password !== state[0].passwordConfirm) {
+      showToast('❗ 비밀번호를 다시 확인해주세요.', 'danger');
+      return false;
+    }
+
+    if (state[0].number < 1 || state[0].number > 30) {
+      showToast('❗ 학생 번호는 1~30 이내에서만 선택할 수 있습니다.', 'danger');
+      return false;
+    }
+
+    if (!check[0].privacy || !check[0].tos) {
+      showToast('❗ 개인정보처리방침 및 이용약관 동의가 필요합니다.', 'danger');
+      return false;
+    }
+
+    return true;
+  };
+
+  const onRegisterClick = () => {
+    if (!validator()) return;
+
+    // TODO: Register API
+  };
+
   return (
     <Container>
       <div>
@@ -41,15 +117,15 @@ const RegisterPage: React.FC = () => {
         <BlankLine gap={30} />
 
         <Form>
-          <RegisterForm />
+          <RegisterForm state={state} />
 
-          <RegisterFooterText />
+          <RegisterFooterText check={check} />
         </Form>
 
         <BlankLine gap={30} />
 
         <ButtonWrapper>
-          <HugeButton>회원가입</HugeButton>
+          <HugeButton onClick={onRegisterClick}>회원가입</HugeButton>
         </ButtonWrapper>
       </div>
     </Container>
