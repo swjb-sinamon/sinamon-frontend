058938ae7fa70b5b38b539c60cf460df5cd27586
combbm <combbm@naver.com>
2020-10-04 01:15:42 +0900

    feat: add view text about user info

--- src/components/MainTitleBar/MyInfoTitle.tsx
+++ src/components/MainTitleBar/MyInfoTitle.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Heading3 } from '../../atomics/Typography/Heading';
 import SCREEN_SIZE from '../../styles/screen-size';
+import { useProfile } from '../../hooks/useProfile';
 
 const AboutContainer = styled.div`
   text-align: center;
@@ -12,10 +13,27 @@ const AboutContainer = styled.div`
 `;
 
 const MyInfoTitle: React.FC = () => {
+  const profile = useProfile();
+
+  if (!profile) {
+    return (
+      <AboutContainer>
+        <Heading3>수정과</Heading3>
+        <Heading3>불러오는 중...</Heading3>
+      </AboutContainer>
+    );
+  }
+
+  const rule = profile.isTeacher ? '선생님' : '학생';
+
   return (
     <AboutContainer>
-      <Heading3>[학과]과 [학년]학년 [반]반</Heading3>
-      <Heading3>[이름] 님</Heading3>
+      <Heading3>
+        [학과]과 {profile.studentGrade}학년 {profile.studentClass}반
+      </Heading3>
+      <Heading3>
+        {profile.name} {rule}
+      </Heading3>
     </AboutContainer>
   );
 };
