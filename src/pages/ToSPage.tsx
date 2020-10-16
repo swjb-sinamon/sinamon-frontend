9247f6f100c6bd5f0c1bffffbc7c76c694e8c472
SkyLightQP <combbm@naver.com>
2020-10-16 15:14:35 +0900

    fix: fix padding style of Terms of Service page

--- src/pages/ToSPage.tsx
+++ src/pages/ToSPage.tsx
@@ -9,8 +9,7 @@ const Container = styled.div`
   width: 1000px;
   margin: 0 auto;
 
-  padding-top: 1rem;
-  padding-left: 1rem;
+  padding: 1rem;
 
   @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
     width: 600px;
@@ -28,14 +27,8 @@ const ToSPage: React.FC = () => {
       <PrivacySubTitle>이용약관</PrivacySubTitle>
       <BlankLine gap={20} />
       <div>
-        <Heading2>1. 개인정보의 처리 목적</Heading2>
-        <p>
-          &lt;수정과&gt;(‘sinamon.info’이하 ‘수정과’) 은(는) 다음의 목적을 위하여 개인정보를
-          처리하고 있으며, 다음의 목적 이외의 용도로는 이용하지 않습니다.
-        </p>
-        <p>
-          - 고객 가입의사 확인, 고객에 대한 서비스 제공에 따른 본인 식별.인증, 회원자격 유지.관리 등
-        </p>
+        <Heading2>1. 제목</Heading2>
+        <p>내용</p>
       </div>
       <BlankLine gap={10} />
     </Container>
