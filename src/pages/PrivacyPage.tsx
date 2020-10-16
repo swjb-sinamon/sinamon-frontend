b9767cd2ce5cc80fd7ed85a98a4fd27fd21dd68a
SkyLightQP <combbm@naver.com>
2020-10-16 14:39:53 +0900

    feat: create PrivacyTitle atomic

--- src/pages/PrivacyPage.tsx
+++ src/pages/PrivacyPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Heading1, Heading2 } from '../atomics/Typography/Heading';
+import { Heading2 } from '../atomics/Typography/Heading';
 import SCREEN_SIZE from '../styles/screen-size';
 import BlankLine from '../utils/BlankLine';
+import { PrivacySubTitle, PrivacyTitle } from '../atomics/Typography/PrivacyTitle';
 
 const Container = styled.div`
   width: 1000px;
@@ -20,35 +21,11 @@ const Container = styled.div`
   }
 `;
 
-const Title = styled.h1`
-  color: transparent;
-  font-size: 3.5rem;
-  -webkit-text-stroke-width: 1.5px;
-  -webkit-text-stroke-color: var(--color-button-hover);
-  opacity: 0.7;
-
-  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_MOBILE}) {
-    font-size: 3rem;
-  }
-`;
-
-const SubTitle = styled(Heading1)`
-  font-size: 2.3rem;
-  margin-top: -1.8rem;
-  margin-left: 3rem;
-
-  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_MOBILE}) {
-    font-size: 1.8rem;
-    margin-top: -1.3rem;
-    margin-left: 2rem;
-  }
-`;
-
 const PrivacyPage: React.FC = () => {
   return (
     <Container>
-      <Title>수정과</Title>
-      <SubTitle>개인정보처리방침</SubTitle>
+      <PrivacyTitle>수정과</PrivacyTitle>
+      <PrivacySubTitle>개인정보처리방침</PrivacySubTitle>
       <BlankLine gap={20} />
       <div>
         <Heading2>1. 개인정보의 처리 목적</Heading2>
