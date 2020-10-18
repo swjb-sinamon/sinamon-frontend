b139ad905961df91de8893c48b7137818c629980
combbm <combbm@naver.com>
2020-10-18 23:40:20 +0900

    feat: add text when data is empty

--- src/components/Card/CalenderCard.tsx
+++ src/components/Card/CalenderCard.tsx
@@ -12,10 +12,14 @@ const Content = styled.pre`
 const CalendarCard: React.FC = () => {
   const { calendar } = useSchool();
 
+  const emptyLength = calendar.filter((value) => value === '').length;
+
   return (
     <Card columnStart={1} columnEnd={6} rowStart={3} rowEnd={4}>
       <CardTitle>곧 있을 행사가 궁금해!</CardTitle>
-      <Content>{calendar.map((value) => `${value}\n`)}</Content>
+      <Content>
+        {emptyLength === 5 ? '행사가 없습니다' : calendar.map((value) => `${value}\n`)}
+      </Content>
     </Card>
   );
 };
