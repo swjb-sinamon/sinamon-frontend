583ea1d0d7382582747b2fb0270349b9bdf19c11
combbm <combbm@naver.com>
2020-10-18 23:05:39 +0900

    feat: add new message to use when is night

--- src/utils/Converter/Weather.ts
+++ src/utils/Converter/Weather.ts
@@ -1,6 +1,15 @@
-type ReturnType = '🌞해가 쨍쨍' | '🌧비가 주륵' | '☁흐려요' | '⛄눈이 펑펑' | '알수없음';
+type ReturnType =
+  | '🌞해가 쨍쨍'
+  | '🌧비가 주륵'
+  | '☁흐려요'
+  | '⛄눈이 펑펑'
+  | '알수없음'
+  | '🌙고요한 밤';
 
 const convertWeatherStatusToString = (status: string): ReturnType => {
+  if (status === 'CLEAR' && new Date().getHours() >= 20) {
+    return '🌙고요한 밤';
+  }
   switch (status) {
     case 'CLEAR':
       return '🌞해가 쨍쨍';
