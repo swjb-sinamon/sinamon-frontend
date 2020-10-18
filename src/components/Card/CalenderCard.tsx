313a034984c3a7b5a0faa00c88a76a656c3abf90
combbm <combbm@naver.com>
2020-10-18 23:22:54 +0900

    feat: create CalendarCard component

---
+++ src/components/Card/CalenderCard.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Card from '../../components/Card';
+import CardTitle from '../../atomics/Typography/CardTitle';
+import Api from '../../api';
+
+const Content = styled.pre`
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 14px;
+`;
+
+const CalendarCard: React.FC = () => {
+  const [calendar, setCalendar] = useState<string[]>([]);
+
+  useEffect(() => {
+    Api.get('/school/calender').then((res) => setCalendar(res.data.data));
+  }, []);
+
+  return (
+    <Card columnStart={1} columnEnd={6} rowStart={3} rowEnd={4}>
+      <CardTitle>곧 있을 행사가 궁금해!</CardTitle>
+      <Content>{calendar.map((value) => `${value}\n`)}</Content>
+    </Card>
+  );
+};
+
+export default CalendarCard;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import MainSideBar from '../components/MainSideBar';
 import Card from '../components/Card';
 import CardTitle from '../atomics/Typography/CardTitle';
 import SCREEN_SIZE from '../styles/screen-size';
-import Api from '../api';
 import WeatherCard from '../components/Card/WeatherCard';
 import QRCodeCard from '../components/Card/QRCodeCard';
 import MealCard from '../components/Card/MealCard';
+import CalendarCard from '../components/Card/CalenderCard';
 
 const Container = styled.div`
   display: grid;
@@ -38,18 +38,7 @@ const StyledContentGrid = styled.div`
   }
 `;
 
-const StyledMeal = styled.pre`
-  font-family: 'Noto Sans KR', sans-serif;
-  font-size: 14px;
-`;
-
 const MainPage: React.FC = () => {
-  const [calender, setCalender] = useState<string[]>([]);
-
-  useEffect(() => {
-    Api.get('/school/calender').then((res) => setCalender(res.data.data));
-  }, []);
-
   return (
     <>
       <Container>
@@ -68,10 +57,7 @@ const MainPage: React.FC = () => {
 
             <QRCodeCard />
 
-            <Card columnStart={1} columnEnd={6} rowStart={3} rowEnd={4}>
-              <CardTitle>곧 있을 행사가 궁금해!</CardTitle>
-              <StyledMeal>{calender.map((value) => `${value}\n`)}</StyledMeal>
-            </Card>
+            <CalendarCard />
           </StyledContentGrid>
         </StyledContent>
       </Container>
