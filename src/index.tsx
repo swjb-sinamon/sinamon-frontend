33e68b8645d25eb2447ce186c8b8ae0c5a58bd42
combbm <combbm@naver.com>
2020-10-19 19:06:27 +0900

    feat: add setAppElement() function to use modal

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import dotenv from 'dotenv';
 import ReactNotification from 'react-notifications-component';
+import Modal from 'react-modal';
 import * as serviceWorker from './serviceWorker';
 import Router from './router';
 import { ProfileProvider } from './hooks/useProfile';
@@ -11,6 +12,8 @@ import './styles/global.css';
 import 'react-notifications-component/dist/theme.css';
 
 dotenv.config();
+
+Modal.setAppElement('#root');
 
 ReactDOM.render(
   <React.StrictMode>
