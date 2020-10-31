0504acaa9a61c4d2edf0c4a479854625025c7f62
combbm <combbm@naver.com>
2020-10-31 23:34:03 +0900

    feat: create NoStyleLink atomic

---
+++ src/atomics/NoStyleLink/index.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const NoStyleLink = styled(Link)`
+  color: #333333;
+  text-decoration: none;
+`;
+
+export default NoStyleLink;
