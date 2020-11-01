dbd8f8e3ce751b730f9d6bc8b920cc820094f0cf
combbm <combbm@naver.com>
2020-11-01 22:47:18 +0900

    feat: implement NoticeCard
    상시적으로 바뀌는 공지사항 특성을 고려해 State를 Provider로 씌우지 않았습니다.

--- src/components/Card/NoticeCard.tsx
+++ src/components/Card/NoticeCard.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import Card from '../../components/Card';
 import CardTitle from '../../atomics/Typography/CardTitle';
+import { Heading3 } from '../../atomics/Typography/Heading';
+import Api from '../../api';
+
+const StyledNotice = styled.div`
+  display: flex;
+  height: 60%;
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+`;
 
 const NoticeCard: React.FC = () => {
+  const [notice, setNotice] = useState<string>('');
+
+  useEffect(() => {
+    Api.get('/notice').then((res) => {
+      if (res.data && res.data.success) setNotice(res.data.data);
+    });
+  }, []);
+
   return (
     <Card columnStart={1} columnEnd={4} rowStart={3} rowEnd={4}>
       <CardTitle>
@@ -11,7 +31,9 @@ const NoticeCard: React.FC = () => {
         </span>
         알려드려요!
       </CardTitle>
-      <p>[공지사항]</p>
+      <StyledNotice>
+        <Heading3>{notice}</Heading3>
+      </StyledNotice>
     </Card>
   );
 };
