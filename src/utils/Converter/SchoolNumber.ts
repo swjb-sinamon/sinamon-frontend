b5e9f267c3d83fc30fc08cb8e1b9422547baf5c5
combbm <combbm@naver.com>
2020-12-22 19:27:26 +0900

    feat: add convertClassToFullClass() util function
    To convert class of student from class(1~2) to fullClass(1~9)

--- src/utils/Converter/SchoolNumber.ts
+++ src/utils/Converter/SchoolNumber.ts
@@ -49,3 +49,16 @@ export const convertClassToDepartment = (clazz: number): [number, number] => {
 
   return [department, realClass];
 };
+
+export const convertClassToFullClass = (department: number, clazz: number): number => {
+  if (department === 1 && clazz === 1) return 1;
+  if (department === 1 && clazz === 2) return 2;
+  if (department === 2 && clazz === 1) return 3;
+  if (department === 2 && clazz === 2) return 4;
+  if (department === 3 && clazz === 1) return 5;
+  if (department === 3 && clazz === 2) return 6;
+  if (department === 4 && clazz === 2) return 7;
+  if (department === 4 && clazz === 2) return 8;
+  if (department === 5 && clazz === 1) return 9;
+  return 0;
+};
