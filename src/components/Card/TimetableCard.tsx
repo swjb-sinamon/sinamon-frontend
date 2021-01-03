678cd9fd8cafcccec2b50a9f07e2be290a73e063
combbm <combbm@naver.com>
2021-01-03 15:24:15 +0900

    fix: 임시로 시간표 기능 비활성화

--- src/components/Card/TimetableCard.tsx
+++ src/components/Card/TimetableCard.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Card, CardTitle, NoStyleLink, SCREEN_SIZE } from 'sinamon-sikhye';
-import TimetableItem from '../Timetable/TimetableItem';
-import useWindowSize from '../../hooks/useWindowSize';
-import { useTimetable } from '../../hooks/useTimetable';
+import { Card, CardTitle, SCREEN_SIZE } from 'sinamon-sikhye';
 
 const CardHeader = styled.div`
   display: flex;
@@ -15,17 +12,17 @@ const LeftHeader = styled.div`
   flex-direction: row;
 `;
 
-const StyledTipText = styled.span`
-  font-size: 14px;
-  color: var(--color-subtext);
-
-  margin-left: 10px;
-  line-height: 2.5;
-
-  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
-    display: none;
-  }
-`;
+// const StyledTipText = styled.span`
+//   font-size: 14px;
+//   color: var(--color-subtext);
+//
+//   margin-left: 10px;
+//   line-height: 2.5;
+//
+//   @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
+//     display: none;
+//   }
+// `;
 
 const TimeTableContainer = styled.div`
   display: grid;
@@ -40,63 +37,63 @@ const TimeTableContainer = styled.div`
   }
 `;
 
-const MoreButton = styled.button`
-  width: 60px;
-  height: 30px;
-  border: 1px var(--color-gray) solid;
-  border-radius: 50px;
-  background-color: white;
-  cursor: pointer;
-
-  &:hover {
-    background-color: var(--color-gray);
-  }
-
-  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
-    display: none;
-  }
-`;
-
-const day = ['일', '월', '화', '수', '목', '금', '토'];
-const today = new Date().getDay();
+// const MoreButton = styled.button`
+//   width: 60px;
+//   height: 30px;
+//   border: 1px var(--color-gray) solid;
+//   border-radius: 50px;
+//   background-color: white;
+//   cursor: pointer;
+//
+//   &:hover {
+//     background-color: var(--color-gray);
+//   }
+//
+//   @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
+//     display: none;
+//   }
+// `;
+//
+// const day = ['일', '월', '화', '수', '목', '금', '토'];
+// const today = new Date().getDay();
 
 const TimetableCard: React.FC = () => {
-  const [width] = useWindowSize();
-  const tablet = parseInt(SCREEN_SIZE.SCREEN_TABLET.replace('px', ''), 10);
-
-  const timetable = useTimetable();
-
-  const TimetableForPC = timetable.map((value, index) => (
-    <TimetableItem
-      // eslint-disable-next-line react/no-array-index-key
-      key={index}
-      day={`${day[index + 1]}`}
-      data={value}
-      active={index + 1 === today}
-    />
-  ));
-
-  const TimetableForMobile = () => {
-    if (today === 0 || today === 6) {
-      return (
-        <>
-          <p>오늘은 주말입니다.</p>
-          <p>
-            더 자세한 시간표는{' '}
-            <NoStyleLink to="/timetable">
-              <b>
-                <u>시간표 보기</u>
-              </b>
-            </NoStyleLink>{' '}
-            메뉴를 <br />
-            이용해주세요.
-          </p>
-        </>
-      );
-    }
-
-    return <TimetableItem day={`${day[today]}`} data={timetable[today - 1]} active />;
-  };
+  // const [width] = useWindowSize();
+  // const tablet = parseInt(SCREEN_SIZE.SCREEN_TABLET.replace('px', ''), 10);
+  //
+  // const timetable = useTimetable();
+  //
+  // const TimetableForPC = timetable.map((value, index) => (
+  //   <TimetableItem
+  //     // eslint-disable-next-line react/no-array-index-key
+  //     key={index}
+  //     day={`${day[index + 1]}`}
+  //     data={value}
+  //     active={index + 1 === today}
+  //   />
+  // ));
+  //
+  // const TimetableForMobile = () => {
+  //   if (today === 0 || today === 6) {
+  //     return (
+  //       <>
+  //         <p>오늘은 주말입니다.</p>
+  //         <p>
+  //           더 자세한 시간표는{' '}
+  //           <NoStyleLink to="/timetable">
+  //             <b>
+  //               <u>시간표 보기</u>
+  //             </b>
+  //           </NoStyleLink>{' '}
+  //           메뉴를 <br />
+  //           이용해주세요.
+  //         </p>
+  //       </>
+  //     );
+  //   }
+  //
+  //   return <TimetableItem day={`${day[today]}`} data={timetable[today - 1]} active />;
+  // };
 
   return (
     <Card columnStart={1} columnEnd={5} rowStart={2} rowEnd={3}>
@@ -108,17 +105,17 @@ const TimetableCard: React.FC = () => {
             </span>
             무엇을 배울까?
           </CardTitle>
-          <StyledTipText>과목명을 클릭하여 줌에 접속하세요</StyledTipText>
+          {/* <StyledTipText>과목명을 클릭하여 줌에 접속하세요</StyledTipText> */}
         </LeftHeader>
 
-        <NoStyleLink to="/timetable">
-          <MoreButton>더보기</MoreButton>
-        </NoStyleLink>
+        {/* <NoStyleLink to="/timetable"> */}
+        {/*  <MoreButton>더보기</MoreButton> */}
+        {/* </NoStyleLink> */}
       </CardHeader>
-
       <TimeTableContainer>
-        {width > tablet ? TimetableForPC : <TimetableForMobile />}
+        {/* {width > tablet ? TimetableForPC : <TimetableForMobile />} */}
       </TimeTableContainer>
+      준비중
     </Card>
   );
 };
--- src/components/MainSideBar/index.tsx
+++ src/components/MainSideBar/index.tsx
@@ -64,13 +64,11 @@ const MainSideBar: React.FC = () => {
           </MainSideBarItem>
         </NoStyleLink>
 
-        <NoStyleLink to="/timetable">
-          <MainSideBarItem>
-            <FontAwesomeIcon icon={faBook} size="lg" />
-            &nbsp;
-            <p>시간표 보기</p>
-          </MainSideBarItem>
-        </NoStyleLink>
+        <MainSideBarItem tabIndex={0} onClick={() => showToast('준비중입니다.', 'info')}>
+          <FontAwesomeIcon icon={faBook} size="lg" />
+          &nbsp;
+          <p>시간표 보기</p>
+        </MainSideBarItem>
 
         <MainSideBarItem tabIndex={0} onClick={() => showToast('준비중입니다.', 'info')}>
           <FontAwesomeIcon icon={faSchool} size="lg" />
