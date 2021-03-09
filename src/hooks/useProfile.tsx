aece7b4ec175cdd242db64c66e036506f63993ac
combbm <combbm@naver.com>
2021-03-09 13:31:35 +0900

    fix: 토큰 등록 시점을 로그인 시점으로 변경

--- src/hooks/useProfile.tsx
+++ src/hooks/useProfile.tsx
@@ -8,10 +8,20 @@ export const ProfileProvider: React.FC = ({ children }) => {
   const [profile, setProfile] = useState<ProfileType | undefined>(undefined);
 
   useEffect(() => {
+    const currentToken = localStorage.getItem('fcm_token');
+
     Api.get('/auth/me').then((res) => {
-      if (!res.data) return;
-      if (!res.data.success) return;
+      if (!res.data) return Promise.reject();
+      if (!res.data.success) return Promise.reject();
       setProfile(res.data.data);
+
+      if (!currentToken) return Promise.reject();
+
+      return Api.post('/fcm', {
+        token: currentToken
+      });
+    }).then(() => {
+      return Api.post('/fcm/subscribe');
     });
   }, []);
 
