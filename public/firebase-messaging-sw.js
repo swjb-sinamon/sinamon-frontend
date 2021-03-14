3617cfad3ae3f7201afd4e151c656ff1f9b139d5
combbm <combbm@naver.com>
2021-03-14 21:55:37 +0900

    feat: add click event into background

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-restricted-globals */
+
 importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.5/firebase-messaging.js');
 
@@ -12,3 +15,21 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const { title } = payload.notification;
+  const options = {
+    body: payload.notification.body,
+    icon: '/logo192.png',
+    data: {
+      click_action: payload.data.click_action
+    }
+  };
+
+  return self.registration.showNotification(title, options);
+});
+
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow(e.notification.data.click_action || 'https://sinamon.info'));
+});
