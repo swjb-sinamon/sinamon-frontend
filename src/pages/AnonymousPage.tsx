17106848ce17c76b6e9c658dac65d3b669463b22
김도현 <pte0001@naver.com>
2021-04-13 11:19:05 +0900

    feat: AnonymousList style

--- src/pages/AnonymousPage.tsx
+++ src/pages/AnonymousPage.tsx
@@ -14,6 +14,23 @@ import MainSideBar from '../components/MainSideBar';
 const StyledContent = styled.div`
   margin: 3rem;
 `;
+const AnonymousList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  text-align: center;
+
+  grid-gap: 2rem;
+
+  width: 1000px;
+  
+  padding: 1rem;
+
+  background-color: white;
+  border-radius: 3px;
+  border: 1px solid var(--color-gray);
+
+  
+`;
 
 const AnonymousPage: React.FC = () => {
 
@@ -27,6 +44,10 @@ const AnonymousPage: React.FC = () => {
         <StyledContent>
           <Heading1>익명 페이지</Heading1>
           <Heading3>익명으로 글을 작성하실 수 있습니다</Heading3>
+          <BlankLine gap={30} />
+          <AnonymousList>
+
+          </AnonymousList>
         </StyledContent>
       </MainSideBarContainer>
     </>
